6bdacb832b7864f0ec104257b2601ae96b8c2ded
vincentkhuang <vincent.kim.huang@gmail.com>
2026-08-18 20:16:45 -0700

    Keep Typewriter one color per run

--- TypewriterPattern.cpp
+++ TypewriterPattern.cpp
@@ -1,29 +1,35 @@
 #include "TypewriterPattern.h"
 
-#include "PatternColors.h"
 #include "PatternTiming.h"
 #include "SignLayout.h"
 
+namespace {
+uint8_t nextTypewriterHue = 128;
+constexpr uint8_t TYPEWRITER_HUE_STEP = 43;
+}  // namespace
+
 void runTypewriterPattern() {
+  CRGB runColor = CHSV(nextTypewriterHue, 235, 255);
+  nextTypewriterHue += TYPEWRITER_HUE_STEP;
+
   clearAllLetters();
   fill_solid(Strip_U, NUM_U, CRGB::Black);
-  fill_solid(Strip_F, NUM_F, CRGB::Purple);
+  fill_solid(Strip_F, NUM_F, CRGB::Black);
 
   for (uint8_t letter = 0; letter < LETTER_COUNT; ++letter) {
-    CRGB letterColor = signRainbowColor(letter, LETTER_COUNT, 128);
     fill_solid(Strip_U, NUM_U, CRGB::Black);
     fillUnderlineSegment(letter, CRGB::White);
     LEDS.show();
     patternDelay(120);
 
-    fillLetter(letter, letterColor);
-    fillUnderlineSegment(letter, letterColor);
+    fillLetter(letter, runColor);
+    fillUnderlineSegment(letter, runColor);
     LEDS.show();
     patternDelay(260);
   }
 
-  fillSignRainbow(Strip_U, NUM_U, 128);
-  fill_solid(Strip_F, NUM_F, CRGB::Magenta);
+  fill_solid(Strip_U, NUM_U, runColor);
+  fill_solid(Strip_F, NUM_F, runColor);
   LEDS.show();
   patternDelay(1400);
 }
